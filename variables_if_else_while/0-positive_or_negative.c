b636daca546e5179657f41790142916c52756a11
Evez Memmedov <evezm254@gmail.com>
2025-06-13 13:21:57 +0000

    Added

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	n = rand() % 201 - 100;
+	printf("%d\n", n);
+	if (n > 0)
+		printf("is positive\n");
+	else if (n == 0)
+		printf("is zero\n");
+	else
+		printf("is negative\n");
+
+	return (0);
+}
