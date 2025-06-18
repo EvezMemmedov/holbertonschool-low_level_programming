652e4b94d33079fe64f6fcd4a6c6968f29708ece
Evez Memmedov <evezm254@gmail.com>
2025-06-18 05:22:35 +0000

    Added

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -7,6 +7,10 @@ int _isupper(int c);
 int _putchar(char c);
 #include <stdio.h>
 #include <stdlib.h>
+<<<<<<< HEAD
+=======
+void print_line(int n);
+>>>>>>> 3d73660 (Added)
 void more_numbers(void);
 void print_numbers(void);
 int mul(int a, int b);
