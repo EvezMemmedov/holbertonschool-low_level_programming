8e530a8373585faf11ed5ec4659f3c8eea1c2588
Evez Memmedov <evezm254@gmail.com>
2025-06-20 11:48:23 +0000

    Added

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - that prints a string, followed by a new line, to stdout.
+ * @str: The string to print.
+ *
+ * Return: Nothing(void).
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 
-
+void _puts(char *str);
 int _strlen(char *s);
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
