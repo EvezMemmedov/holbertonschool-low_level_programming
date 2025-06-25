d2708ece58473e6dc2504164c24be658a39b9ef9
Evez Memmedov <evezm254@gmail.com>
2025-06-25 12:35:49 +0000

    Added

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 
-
+void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
