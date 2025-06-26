5531f98e24177df887ee75b164c763f927ff16f5
Evez Memmedov <evezm254@gmail.com>
2025-06-26 08:39:22 +0000

    Added

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,23 +1,30 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion -  natural square root of a number
- * @n: square root
+ * _sqrt_helper - rekursiv köməkçi funksiya
+ * @n: kökü tapılacaq ədəd
+ * @i: test üçün olan ədəd (təxmin)
  *
- * Return: i and -1
+ * Return: n-in təbii kvadrat kökü və ya -1
+ */
+int _sqrt_helper(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (_sqrt_helper(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - n-in təbii kvadrat kökünü tapır (rekursiv)
+ * @n: ədəd
+ *
+ * Return: kvadrat kök və ya -1
  */
 int _sqrt_recursion(int n)
 {
-	int i = 0;
-	
 	if (n < 0)
 		return (-1);
-
-	while (i * i <= n)
-	{
-		if (i * i == n)
-			return (i);
-		i++;
-	}
-	return (-1);
+	return (_sqrt_helper(n, 0));
 }
