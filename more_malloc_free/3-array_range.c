13f57955dd1a3d6d12528d485d675d008fc780c6
Evez Memmedov <evezm254@gmail.com>
2025-07-03 10:06:51 +0000

    Adde

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -21,7 +21,7 @@ int *array_range(int min, int max)
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i <= max -min; i++)
+	for (i = 0; i <= max - min; i++)
 		arr[i] = min + i;
 
 	return (arr);
