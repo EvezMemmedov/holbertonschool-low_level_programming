9438e2cc14d739ad6a3171df4565e6ce12c27528
Evez Memmedov <evezm254@gmail.com>
2025-07-03 09:23:33 +0000

    Adde

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,6 +13,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i = 0, j = 0, x;
 	char *ptr;
+
 	if (s2 == NULL)
 		s2 = "";
 
@@ -21,19 +22,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	while (s1[i] != '\0')
 		i++;
+
 	while (s2[j] != '\0')
 		j++;
+
 	if (n < j)
 		j = n;
 
 	ptr = malloc(i + j + 1);
+
 	if (ptr == NULL)
 		return (NULL);
 
 	for (x = 0; x < i; x++)
 		ptr[x] = s1[x];
+
 	for (x = 0; x < j; x++)
 		ptr[x + i] = s2[x];
+
 	ptr[i + j] = '\0';
 
 	return (ptr);
