023a5ccb3fa2694afabb577e6b9306e82ecf8b67
Evez Memmedov <evezm254@gmail.com>
2025-07-05 17:15:54 +0000

    diHDI

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,22 +2,22 @@
 #include "dog.h"
 
 /**
- * _strlen - calculates the length of a string
- * @s: the string
+ * _strlen - returns the length of a string
+ * @s: string to measure
  *
- * Return: length
+ * Return: length of string
  */
 int _strlen(const char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i])
-        i++;
-    return (i);
+	while (s[i])
+		i++;
+	return (i);
 }
 
 /**
- * _strcpy - copies string from src to dest
+ * _strcpy - copies a string from src to dest
  * @dest: destination buffer
  * @src: source string
  *
@@ -25,59 +25,59 @@ int _strlen(const char *s)
  */
 char *_strcpy(char *dest, const char *src)
 {
-    int i = 0;
+	int i = 0;
 
-    while (src[i])
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-    return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
 
 /**
- * new_dog - creates a new dog
- * @name: dog's name
- * @age: dog's age
- * @owner: dog's owner
+ * new_dog - creates a new dog struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  *
- * Return: pointer to new dog (dog_t), or NULL on failure
+ * Return: pointer to new dog (dog_t), or NULL if failure
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *d;
-    char *n_copy, *o_copy;
-    int name_len, owner_len;
+	dog_t *d;
+	char *n_copy, *o_copy;
+	int name_len, owner_len;
 
-    d = malloc(sizeof(dog_t));
-    if (d == NULL)
-        return (NULL);
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
 
-    name_len = _strlen(name);
-    owner_len = _strlen(owner);
+	name_len = _strlen(name);
+	owner_len = _strlen(owner);
 
-    n_copy = malloc(name_len + 1);
-    if (n_copy == NULL)
-    {
-        free(d);
-        return (NULL);
-    }
+	n_copy = malloc(name_len + 1);
+	if (n_copy == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
 
-    o_copy = malloc(owner_len + 1);
-    if (o_copy == NULL)
-    {
-        free(n_copy);
-        free(d);
-        return (NULL);
-    }
+	o_copy = malloc(owner_len + 1);
+	if (o_copy == NULL)
+	{
+		free(n_copy);
+		free(d);
+		return (NULL);
+	}
 
-    _strcpy(n_copy, name);
-    _strcpy(o_copy, owner);
+	_strcpy(n_copy, name);
+	_strcpy(o_copy, owner);
 
-    d->name = n_copy;
-    d->age = age;
-    d->owner = o_copy;
+	d->name = n_copy;
+	d->age = age;
+	d->owner = o_copy;
 
-    return (d);
+	return (d);
 }
