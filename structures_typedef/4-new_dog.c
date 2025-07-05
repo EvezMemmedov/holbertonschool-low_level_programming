7e97d359633dca3e53c3aea49bb9e30d968eb247
Evez Memmedov <evezm254@gmail.com>
2025-07-05 17:06:44 +0000

    diHDI

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to new dog (dog_t), or NULL if failure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	char *n_copy, *o_copy;
+
+	d = malloc(sizeof(dog_t));	
+	if (d == NULL)
+		return (NULL);
+
+	n_copy = malloc(strlen(name) + 1);
+	if (n_copy == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+	strcpy(n_copy, name);
+
+	o_copy = malloc(strlen(name) + 1);
+	if (o_copy == NULL)
+	{
+		free(d);
+		free(n_copy);
+		return (NULL);
+	}
+	strcpy(o_copy, owner);
+
+	d->name = n_copy;
+	d->age = age;
+	d->owner = o_copy;
+
+	return(d);
+}
