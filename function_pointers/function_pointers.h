6e2d3ef2eadfb46929384204b00c45204abdee84
Evez Memmedov <evezm254@gmail.com>
2025-07-07 12:44:40 +0000

    Added

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -3,13 +3,11 @@
 
 #include <stddef.h>
 
-/**
- * print_name - Prints a name using a function pointer
- * @name: The name to print
- * @f: The function used to print the name
- */
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+
 /**
  * _putchar - Writes a character to stdout
  * @c: The character to print
