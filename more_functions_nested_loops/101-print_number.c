6d8f11991b40b52d9650105b9f81d1b9547e0cf8
Evez Memmedov <evezm254@gmail.com>
2025-07-17 09:01:24 +0000

    Added

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * print_number - hat prints an integer.
  * @n: number
@@ -11,10 +12,14 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		if (n / 10)
+			print_number(-(n / 10));
+		_putchar('0' - (n % 10));
 	}
-	if (n / 10)
-		print_number(n / 10);
-
-	_putchar((n % 10) + '0');
+	else
+	{
+		if (n / 10)
+			print_number(n / 10);
+		_putchar((n % 10) + '0');
+	}
 }
