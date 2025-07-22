f53b0dbec693ea49a4b95dd928ebebbf24fef679
Evez Memmedov <evezm254@gmail.com>
2025-07-22 11:57:49 +0000

    Added

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -15,11 +15,13 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		count++;
+
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", (unsigned int)h->len, h->str);
 		h = h->next;
 	}
+
 	return (count);
 }
