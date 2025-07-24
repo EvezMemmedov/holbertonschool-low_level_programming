c791b3029b58d0de0f3bf5ed2c1536f5c74a56e5
Evez Memmedov <evezm254@gmail.com>
2025-07-24 14:34:32 +0000

    Added

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - that adds a new node at the beginning
+ * @head: head
+ * @n: integer
+ *
+ * Return: new_node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	
+	new_node->n = n;
+
+	new_node->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new_node;
+	new_node->prev = NULL;
+	*head = new_node;
+	return (new_node);
+}
