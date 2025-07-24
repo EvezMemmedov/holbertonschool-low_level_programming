f6273529c9c3819236e64789217ac18a4aa7b915
Evez Memmedov <evezm254@gmail.com>
2025-07-24 13:48:10 +0000

    Added

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - hat prints all the elements
+ * @h: pointer to the head of the list
+ *
+ * Return: count
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
