17719b4f65d4ff42082d753e8bff2ce6e86f112f
Evez Memmedov <evezm254@gmail.com>
2025-07-26 16:16:16 +0000

    sdacc

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -14,7 +14,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i;
 	dlistint_t *current = *head;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
 	if (index == 0)
