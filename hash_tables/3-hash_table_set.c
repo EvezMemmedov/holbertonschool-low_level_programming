34922cf858bd5113e42970b593ddc3fc3c639373
Evez Memmedov <evezm254@gmail.com>
2025-07-27 16:13:51 +0000

    Added

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_table_t *new_node, *temp;
+	hash_node_t *new_node, *temp;
 	unsigned long int index;
 
 	if (!ht || !key || *key == '\0' || !value)
